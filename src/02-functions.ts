89da426145a9b44e601c40d2620443b294ff296f
424524Ai <hleabrszylx@gmail.com>
2026-09-14 12:02:01 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -68,3 +68,16 @@ function findFriends(
 }
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+console.log("----------------------------");
+
+function addInterest(
+    friend: Friend,
+    interest: string
+): string[]{
+    if(!friend.interests){
+        friend.interests = []
+    }
+    friend.interests?.push(interest);
+    return friend.interests;
+}
+console.log(addInterest(friends[1], 'Politics'))
